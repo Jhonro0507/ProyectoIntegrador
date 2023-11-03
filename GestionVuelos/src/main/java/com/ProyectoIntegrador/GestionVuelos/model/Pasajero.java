c5d9fd121c5d0bae4b50377cd80f8b6839beab6a
Jhonatan Román Román <jhonatanromanroman@MacBook-Pro-de-Jhonatan.local>
2023-11-02 21:44:46 -0500

    Edición de clases

--- GestionVuelos/src/main/java/com/ProyectoIntegrador/GestionVuelos/model/Pasajero.java
+++ GestionVuelos/src/main/java/com/ProyectoIntegrador/GestionVuelos/model/Pasajero.java
@@ -1,4 +1,27 @@
 package com.ProyectoIntegrador.GestionVuelos.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "pasajeros")
 public class Pasajero {
+    private String codPas;
+    private String dni;
+    private String nombre;
+    private String apellidoP;
+    private String apellidoM;
+    private String tipoPasajero;
+    private String billete;
+    private double tasas;
+    private String cg;
+    private String ge;
+    private double difTarifaNacional;
+
 }
--- GestionVuelos/src/main/java/com/ProyectoIntegrador/GestionVuelos/model/Vuelo.java
+++ GestionVuelos/src/main/java/com/ProyectoIntegrador/GestionVuelos/model/Vuelo.java
@@ -1,4 +1,36 @@
 package com.ProyectoIntegrador.GestionVuelos.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "vuelos")
 public class Vuelo {
+    private LocalDate opFecha;
+    private String opDes;
+    private String codOp;
+    private LocalDate fechaSalida;
+    private String iOrigen;
+    private String iDestino;
+    private LocalDate iFechaLlegada;
+    private String iNBillete;
+    private String iAerolinea;
+    private String nacional;
+    private String proveedor;
+    private String notas;
+    private LocalDate dFechaSalida;
+    private LocalDate dOrigen;
+    private String dDestino;
+    private LocalDate dFechaLlegada;
+    private String dNBillete;
+    private String dAerolinea;
 }
