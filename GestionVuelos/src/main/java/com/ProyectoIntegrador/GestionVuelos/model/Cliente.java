2122fa4628f9c776cf61b65afde582df39e4cb7d
KeviinBL <kevin.bejarano00@gmail.com>
2023-11-03 11:33:26 -0500

    clien

---
+++ GestionVuelos/src/main/java/com/ProyectoIntegrador/GestionVuelos/model/Cliente.java
@@ -0,0 +1,16 @@
+package com.ProyectoIntegrador.GestionVuelos.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "cliente")
+
+public class Cliente {
+}
